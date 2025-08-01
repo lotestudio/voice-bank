f2a8bac65f937bacf775fdd388eae09dcbb13370
lotestudio <info@lote.bg>
2025-08-01 10:45:47 +0300

    fix sidebar

--- resources/js/layouts/SiteLayout.vue
+++ resources/js/layouts/SiteLayout.vue
@@ -1,6 +1,6 @@
 <template>
 <SiteShell>
-    <MobileSidebar />
+    <MobileSidebar/>
     <Header/>
     <main class="mx-auto flex h-full w-full max-w-7xl flex-1 flex-col gap-4 rounded-xl">
         <slot />
@@ -12,23 +12,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import {Link, usePage } from '@inertiajs/vue3';
-import { useLocale } from '@/composables/useLocale.js';
 import SiteShell from '@/components/Site/SiteShell.vue';
 import MobileSidebar from '@/components/Site/MobileSidebar.vue';
 import Header from '@/components/Site/Header.vue';
-
-const page = usePage();
-
-const locale = ref(page.props.locale);
-
-const contentTrigger = ref(false);
-
-const { T } = useLocale()
-
-onMounted(() => {
-    contentTrigger.value = true;
-});
-
 </script>
